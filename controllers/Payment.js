83f5dbc069f5030d9d99a45f3e8baf12aee5804c
kingCoders60 <spworld30@gmail.com>
2024-10-15 21:01:43 +0530

    Automatic sync: Tue, Oct 15, 2024  9:01:43 PM

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -1,1 +1,6 @@
-const {instance} = require()
+const {instance} = require("../config/razorpay");
+const Course = require("../models/Course");
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+const {courseEnrollmentEmail} = require("../mail/template/courseEnrollmentEmail");
+
